9206fcf818d3fa5eefc50e5ac8c96e2ee4684326
Maiztegui <web23nmaiztegi@imaltuna.com>
2024-03-01 12:26:36 +0100

    .

--- Lehen webgunea/Models/Contact.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Lehen_webgunea.Models
-{
-    public class Contact
-    {
-        public int id { get; set; }
-    }
-}
---
+++ Lehen webgunea/Models/Table.cs
@@ -0,0 +1,10 @@
+﻿namespace Lehen_webgunea.Models
+{
+    public class Table
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+    }
+}

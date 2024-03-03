ab42e556c9113a53a7502f5f3495d9f09f874021
Maiztegui <web23nmaiztegi@imaltuna.com>
2024-03-03 18:19:21 +0100

    .

--- Lehen webgunea/Models/Table.cs
+++ Lehen webgunea/Models/Table.cs
@@ -1,10 +1,14 @@
-﻿namespace Lehen_webgunea.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lehen_webgunea.Models
 {
     public class Table
     {
+        [Key]
         public int id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
-    }
-}
+ }
+

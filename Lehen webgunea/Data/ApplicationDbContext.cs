31af3698aacb5f9d3011ce37e5b17f2824fb2912
Maiztegui <web23nmaiztegi@imaltuna.com>
2024-03-03 18:59:41 +0100

    conectionString

---
+++ Lehen webgunea/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lehen_webgunea.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}

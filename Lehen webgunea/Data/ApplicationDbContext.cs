4a59d988661332014cf4e1911b118e850d213887
Maiztegui <web23nmaiztegi@imaltuna.com>
2024-03-03 20:57:00 +0100

    create table and get information from server

--- Lehen webgunea/Data/ApplicationDbContext.cs
+++ Lehen webgunea/Data/ApplicationDbContext.cs
@@ -13,6 +13,15 @@ namespace Lehen_webgunea.Data
 
         public DbSet<Category> Categories { get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Category21Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Category21Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Category21Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+
+
     }
 }

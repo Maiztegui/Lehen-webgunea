7193add021b64a3da7c1a91e8e98f22f3885f02a
Maiztegui <web23nmaiztegi@imaltuna.com>
2024-03-10 23:27:28 +0100

    delete

--- Lehen webgunea/Controllers/CategoryController.cs
+++ Lehen webgunea/Controllers/CategoryController.cs
@@ -20,5 +20,82 @@ namespace Lehen_webgunea.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Edit(int? id )
+        {
+            if (id==null || id == 0)
+            {
+                return NotFound();
+
+            }
+            Category? categoryfromDB = _db.Categories.Find(id);
+            //Category? categoryfromDB1 = _db.Categories.FirstOrDefault(u=>u.Category21Id==id);
+            //Category? categoryfromDB2 = _db.Categories.Where(u => u.Category21Id == id).FirstOrDefault();
+            if (categoryfromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+           
+
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            Category? categoryfromDB = _db.Categories.Find(id);
+           
+            if (categoryfromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDB);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

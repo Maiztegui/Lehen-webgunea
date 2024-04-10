c87fa0bf4efd5597b2c22ad80270613ba8454cb5
Maiztegui <webnmaiztegi@imaltuna.com>
2024-04-10 13:42:16 +0200

    5:0

---
+++ Lehen_webgunea.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lehen_webgunea.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        // T- Category
+        IEnumerable<T> GetAll();
+
+    }
+}

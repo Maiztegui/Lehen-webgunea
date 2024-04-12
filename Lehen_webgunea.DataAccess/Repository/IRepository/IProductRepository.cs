03955a2e54e383dc17f9524521fed7d874cf7e42
Maiztegui <webnmaiztegi@imaltuna.com>
2024-04-12 11:34:36 +0200

    5.57

---
+++ Lehen_webgunea.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Lehen_webgunea.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lehen_webgunea.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+      
+    }
+}
---
+++ Lehen_webgunea.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Lehen_webgunea.DataAccess.Data;
+using Lehen_webgunea.DataAccess.Repository.IRepository;
+using Lehen_webgunea.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lehen_webgunea.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base (db)
+        {
+            _db = db;
+        }
+
+        
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
